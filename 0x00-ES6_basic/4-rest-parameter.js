c98e3d16ca21bc7795d8fde7511b80329e9c54eb
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2024-01-18 04:07:19 +0000

    done

--- 0x00-ES6_basic/4-rest-parameter.js
+++
@@ -1,8 +0,0 @@
-/**
- * Counts the number of arguments passed to the function.
- * @param {...any} args - Any number of arguments.
- * @returns {number} - The count of arguments passed.
- */
-export default function returnHowManyArguments(...args) {
-  return args.length;
-}
